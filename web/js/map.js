a2925866c73827c8165167d7ad626ff1d046526a
Tero Parviainen <tero.parviainen@eficode.fi>
2012-08-26 01:23:24 +0300

    threw in jQuery

--- web/js/map.js
+++ web/js/map.js
@@ -1,20 +1,17 @@
-function initialize() {
-	new google.maps.Map(document.getElementById('map'), {
- 		center: new google.maps.LatLng(60.200833, 24.9375),
+// Called by Google Maps once it has loaded everything it needs (specified as ballcack in the Maps API URL)
+window.initGoogleMaps = function() {
+	var mapEl = $('#map')[0];
+	new google.maps.Map(mapEl, {
+ 		center: new google.maps.LatLng(60.200833, 24.9375), // Helsinki
   	zoom: 12,
   	mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
 }
 
-function loadScript() {
-	var url = 'http://maps.googleapis.com/maps/api/js?sensor=true&callback=initialize';
-	if (window.location.hostname !== 'localhost') {
+$(function() {
+	var url = 'http://maps.googleapis.com/maps/api/js?sensor=true&callback=initGoogleMaps';
+	if (window.location.hostname !== 'localhost') { // Don't use API key in local development
 		url += '&key=AIzaSyDZj9_A4WUDGph6cKf2A7VsFbDz6Pb7QBk';
 	}
-	
-	var script = document.createElement('script');
-	script.src = url;
-	document.body.appendChild(script);
-}
-
-window.onload = loadScript;
+	$.getScript(url);
+});
