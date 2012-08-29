1300cbd7f2d680339ad6717545d7adf89f7d493c
Tero Parviainen <tero.parviainen@eficode.fi>
2012-08-29 18:49:42 +0300

    omg a dynamic request handler

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 load('vertx.js');
 
-var webServerConf = {
-	port: 8080
-};
+var server = vertx.createHttpServer(),
+    routeMatcher = new vertx.RouteMatcher();
 
-vertx.deployModule('vertx.web-server-v1.0', webServerConf);
+routeMatcher.get('/ping', function(req) {
+  req.response.end(JSON.stringify({pong: new Date()}));
+});
+
+// TODO: Might want to disable this in production since files are served by Nginx.
+routeMatcher.noMatch(function(req) {
+  var file = '';
+  if (req.path == '/') {
+    file = 'index.html';
+  } else if (req.path.indexOf('..') == -1) {
+    file = req.path;
+  }
+  req.response.sendFile('web/' + file);
+});
+
+
+server.requestHandler(routeMatcher).listen(8080);
