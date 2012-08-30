9802e83cff75e3d12290a31aea656703524f1f9a
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2012-08-30 23:48:18 +0300

    Andd remove the unnecessary dependency.

--- web/js/views/search_view.js
+++ web/js/views/search_view.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore', 'backbone', 'text!templates/search.handlebars'], function ($, _, Backbone, template) {
+define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
 
   return Backbone.View.extend({
 
