1861a5e7b128b2e25943c0f7e2ead94f4a16f706
Tero Parviainen <tero.parviainen@eficode.fi>
2012-08-30 19:37:22 +0300

    Draw a test route from your current location to Kontula

--- web/js/views/map_view.js
+++ web/js/views/map_view.js
@@ -1,4 +1,4 @@
-define(['jquery', 'backbone', 'async!http://maps.googleapis.com/maps/api/js?sensor=true' + (window.location.host === 'localhost' ? '' : '&key=AIzaSyDZj9_A4WUDGph6cKf2A7VsFbDz6Pb7QBk')], function ($, Backbone) {
+define(['jquery', 'underscore', 'backbone', 'async!http://maps.googleapis.com/maps/api/js?sensor=true' + (window.location.host === 'localhost' ? '' : '&key=AIzaSyDZj9_A4WUDGph6cKf2A7VsFbDz6Pb7QBk')], function ($, _, Backbone) {
   return Backbone.View.extend({
 
     el: $('#map'),
@@ -24,9 +24,17 @@ define(['jquery', 'backbone', 'async!http://maps.googleapis.com/maps/api/js?sens
           var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
           that.map.setCenter(latLng);
 
-          // Testing geocoding
-          $.get('address?coords='+latLng.lng()+','+latLng.lat(), function(res) {
-            alert("I know where you live: "+res);
+          // Testing routing
+          $.getJSON('routes?from='+latLng.lng()+','+latLng.lat()+'&to=Leikkikuja 2', function(res) {
+            var shapes = _(res[0][0].legs).map(function(leg) { return leg.shape; })
+              , points = _(shapes).reduce(function(res, shape) { return res.concat(shape); }, [])
+              , latLngs = _(points).map(function(point) { return new google.maps.LatLng(point.y, point.x); });
+            new google.maps.Polyline({
+              map: that.map,
+              path: latLngs,
+              strokeColor: '#0000ee',
+              strokeWeight: 4
+            });
           });
 
         }, function() { /* error callback */ }, {enableHighAccuracy: true});
