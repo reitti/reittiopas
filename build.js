85c9fd412c8a91aac9c9358eeb7334a8a357a9f4
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2012-09-09 17:42:26 +0300

    Optimize build file.

--- build.js
+++ build.js
@@ -3,8 +3,9 @@
     baseUrl: 'js/',
     dir: 'public',
     mainConfigFile: 'web/js/main.js',
-    optimizeCss: 'standard.keepComments',
+    optimizeCss: 'standard',
     inlineText: true,
+    preserveLicenseComments: false,
     modules: [
         { name:'main', include: ['async', 'views/map_view', 'views/search_view'] }
     ]
