ecfbd241382ac8efd078b563a85b573956233f61
Tero Parviainen <tero.parviainen@eficode.fi>
2012-10-07 18:53:48 +0300

    pass -Xmx through JAVA_OPTS

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-vertx run app.coffee -cp "lib/ehcache-core-2.6.0.jar:lib/slf4j-api-1.7.0.jar:lib/slf4j-simple-1.7.0.jar:lib/handlebars-0.5.3.jar:lib/commons-lang3-3.1.jar:lib/parboiled-java-1.0.2.jar:lib/parboiled-core-1.0.2.jar:lib/asm-3.3.1.jar" -Xmx 512M
+JAVA_OPTS="-Xmx1024M" vertx run app.coffee -cp "lib/ehcache-core-2.6.0.jar:lib/slf4j-api-1.7.0.jar:lib/slf4j-simple-1.7.0.jar:lib/handlebars-0.5.3.jar:lib/commons-lang3-3.1.jar:lib/parboiled-java-1.0.2.jar:lib/parboiled-core-1.0.2.jar:lib/asm-3.3.1.jar"
+
